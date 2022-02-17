a75bbded628f44ff68cd6b80f9a55c8bc81fb428
Rahmat Subandi <rahmat@indonesiacrypto.network>
2022-02-18 00:48:54 +0700

    fixing strpos() for PHP 7.3

--- application/third_party/MX/Router.php
+++ application/third_party/MX/Router.php
@@ -233,13 +233,11 @@ class MX_Router extends CI_Router
 		}
 	}
 
-	public function set_class($class)
-	{
+	public function set_class($class) {
 		$suffix = $this->config->item('controller_suffix');
-		if (strpos($class, $suffix) === FALSE)
-		{
-			$class .= $suffix;
+		if ($suffix && strpos($class, $suffix) === FALSE) {
+				$class .= $suffix;
 		}
 		parent::set_class($class);
-	}
+} 
 }	
